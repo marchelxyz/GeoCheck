72a854a81ae3bf5bf7bbe0d53ac3a5201741b8bd
unknown <masovroma@gmail.com>
2026-01-13 12:55:24 +0300

    Исправить синтаксическую ошибку: заменить многострочную строку на строку с \n\n

--- server/index.js
+++ server/index.js
@@ -550,7 +550,7 @@ app.post('/api/check-ins/request', verifyTelegramWebApp, async (req, res) => {
 Пожалуйста, отправьте ваше текущее местоположение (Live Location) и фото.'
 
 
-Пожалуйста, отправьте ваше текущее местоположение (Live Location) и фото.'
+
       );
     } catch (error) {
       console.error('Error sending check-in notification:', error);
